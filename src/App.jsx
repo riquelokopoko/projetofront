ae121847e9e2fc928d7a1eb9e116ed67563c9576
Enrique Cabrera Lopes <78917317+riquelokopoko@users.noreply.github.com>
2023-09-26 01:12:32 +0000

    Carregamento dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+    //tags: ['tag1', 'tag2']
+  }
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+    //tags: ['tag1', 'tag2']
+  }
+
+  const itens = [item1, item2, item1]
+
   return (
     <>
-      <Card />
-      <Card />
+      
+      {itens.map(function (item) {
+        return <Card item={item}/>
+      })}
     </>
   )
 }
