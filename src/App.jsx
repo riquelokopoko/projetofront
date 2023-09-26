f9c1eb90590191740258686f75060f2332d73fb4
Enrique Cabrera Lopes <78917317+riquelokopoko@users.noreply.github.com>
2023-09-26 00:07:44 +0000

    transferencia do css para o do componente

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,7 @@ function App() {
   return (
     <>
       <Card />
-      <div className='card'>
-        <h2>Rick Sanchez</h2>
-        <div className='tags'>
-          <div className='tag'>Status: vivo</div>
-          <div className='tag'>Especie: humana</div>
-          <div className='tag'>Origem: terra C</div>
-
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      <Card />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,14 @@
 import "./Card.css"
 export default function Card() {
-    return <div>Card</div>
+    return <div className='card'>
+    <h2>Rick Sanchez</h2>
+    <div className='tags'>
+      <div className='tag'>Status: vivo</div>
+      <div className='tag'>Especie: humana</div>
+      <div className='tag'>Origem: terra C</div>
+
+    </div>
+    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+  </div>
     
 }
